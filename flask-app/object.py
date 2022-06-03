a5e1397b2a6ec9f4905d3d30299f58f8ed65dd4f
michal64680 <michal.kepinski31@wp.pl>
2022-06-03 22:19:13 +0200

    Added functions for fuzzy logic

--- flask-app/object.py
+++ flask-app/object.py
@@ -7,12 +7,13 @@ from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource
 from bokeh.layouts import row, column, gridplot
 from bokeh.models.widgets import Tabs, Panel
+from fuzzy_logic import set_get_fuzzy_variables
 
 class object:
     def __init__(self) -> None:
         pass
 
-    def change_parameters(self, Tp, t_sim, drag, v_zad, Fp, m, load, Kp, Ti, Td, alpha):
+    def change_parameters_PID(self, Tp, t_sim, drag, v_zad, Fp, m, load, Kp, Ti, Td, alpha):
         self.Tp = Tp
         self.t_sim = t_sim
         self.N = int(self.t_sim/self.Tp)
@@ -27,11 +28,31 @@ class object:
         self.Ti = Ti
         self.Td = Td
         self.alpha = (alpha*2*3.141529/360.0)
-        self.e=[0]
+        self.e=[v_zad]
         self.e_sum=[]
         self.u=[0]
+        self.drag_array = [-(self.m+self.load)*9.81*math.sin(self.alpha)]
 
-        self.__start()
+        self.__start_PID()
+
+    def change_parameters_fuzzy(self, Tp, t_sim, drag, v_zad, Fp, m, load, alpha):
+        self.Tp = Tp
+        self.t_sim = t_sim
+        self.N = int(self.t_sim/self.Tp)
+        self.x_axis=np.arange(0.0,self.t_sim,self.Tp)
+        self.drag = drag
+        self.v = [0]
+        self.v_zad = v_zad
+        self.Fp = Fp
+        self.m = m
+        self.load = load
+        self.alpha = (alpha*2*3.141529/360.0)
+        self.e=[v_zad]
+        self.e_sum=[]
+        self.u=[0]
+        self.drag_array = [-(self.m+self.load)*9.81*math.sin(self.alpha)]
+
+        self.__start_fuzzy()
 
     def __controllerP(self,n):
         return (self.Kp*self.e[n])
@@ -69,11 +90,18 @@ class object:
 
     def __velocity(self,v_n,u_n):
         self.e.append(self.v_zad-v_n)
-        return ((self.Tp/(self.m+self.load))*(self.Fp*self.__limit(u_n)-0.5*self.drag*v_n*v_n-(self.m+self.load)*10*math.sin(self.alpha))+v_n)
+        drag_temp = -0.5*self.drag*v_n*v_n-(self.m+self.load)*9.81*math.sin(self.alpha)
+        self.drag_array.append(drag_temp)
+        return ((self.Tp/(self.m+self.load))*(self.Fp*self.__limit(u_n)+ drag_temp)+v_n)
     
-    def __start(self):
+    def __start_PID(self):
         for i in range(0,self.N-1):
             self.v.append(self.__velocity(self.v[i],self.__controllerPID(i)))
+
+    def __start_fuzzy(self):
+        for i in range(0,self.N-1):
+            u_signal = set_get_fuzzy_variables(self.e[i], self.drag_array[i])
+            self.v.append(self.__velocity(self.v[i], u_signal))
     
     def get_v(self):
         return self.v
@@ -83,7 +111,42 @@ class object:
 
     def get_e(self):
         return self.e
+        
+    def get_drag_array(self):
+        return self.drag_array
 
     def get_x_axis(self):
         return self.x_axis
+
+#Object1 = object()
+#plt.clf()
+#Object1.change_parameters_fuzzy(0.1,500,1.5,50,100,1500,100,0)
+
+#tt = Object1.get_x_axis()
+#v = Object1.get_u()
+
+#plt.plot(tt,v)
+#plt.xlabel("Czas [min]")
+#plt.ylabel("Prędkosc")
+#plt.grid()
+#plt.show()
+
+#Object1.change_parameters_PID(0.1,45,5,99,1000,500,0,0.2,0.15,1.5,-45)
+#
+#tt = Object1.get_x_axis()
+#dd = Object1.get_drag_array()
+#err = Object1.get_e()
+#v = Object1.get_v()
+#u = Object1.get_u()
+#
+##plt.plot(tt,Object1.get_drag_array())
+#plt.plot(tt,dd)
+#plt.plot(tt,err)
+#plt.plot(tt,v)
+#plt.plot(tt,u)
+#plt.xlabel("Czas [min]")
+#plt.ylabel("Prędkosc")
+#plt.grid()
+#plt.show()
+
 
